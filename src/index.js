34280ecb82c9862289a6a4983c251a94b8d389ed
Filipe Moreira <fddcm.daniel@gmail.com>
2025-05-09 18:21:14 +0100

    feat: create a students' report generate function

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import { generateStudentReport } from './report.js';
+
+const students = [
+  { id: 1, name: "João", scores: [20, 15, 19, 18] },
+  { id: 1, name: "Maria", scores: [20, 20, 19, 13] },
+  { id: 1, name: "José", scores: [20, 17, 19, 12] },
+  { id: 1, name: "Ricardo", scores: [10, 2, 3, 12] },
+];
+
+try {
+  const report = generateStudentReport(students, { includeAverage: true });
+
+  console.table(report);
+} catch (error) {
+  console.error("Error: ", error);
+}
