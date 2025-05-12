7cd9379f3d1f4ab6d6c0ee38f67f99ad287a3f54
Filipe Moreira <fddcm.daniel@gmail.com>
2025-05-12 21:51:29 +0100

    feat: Add extra credit value to the average

--- src/report.js
+++ src/report.js
@@ -1,7 +1,7 @@
 /**
  * Generate detailed report objects for each student in the list.
  * Including their ID, name, optional average score, pass status and a summary message.
- * @param {Array<{ id: number, name: string, scores: number[] }>} studentsList
+ * @param {Array<{ id: number, name: string, scores: number[], hasExtraCredit: boolean }>} studentsList
  * @param {{ includeAverage?: boolean }} [options={}]
  * @returns {Array<{ id: number, name: string, average?: number, passed?: string, message?: string }>}
  */
@@ -15,15 +15,35 @@ export function generateStudentReport(studentsList, { includeAverage = true } = 
   const reportList = [];
 
   for (let i = 0; i < studentsList.length; i++) {
-    const { id, name, scores } = studentsList[i];
+    const { id, name, scores, hasExtraCredit } = studentsList[i];
     let average = 0;
+    let totalScores = 0;
 
     if (includeAverage) {
       for (let j = 0; j < scores.length; j++) {
+        if(typeof scores[j] !== 'number' || scores[j] < 0 || scores[j] > 20) {
+          continue;
+        }
+
+        totalScores++;
         average += scores[j];
       }
 
-      average /= scores.length;
+      if(totalScores) {
+        average /= totalScores;
+      } else {
+        average = undefined;
+
+        return;
+      }
+
+      if(hasExtraCredit) {
+        average += 1;
+      }
+
+      if(average > 20) {
+        average = 20;
+      }
     } else {
       average = undefined;
     }
