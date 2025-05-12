f48d34c876017cc5129a812d3f5673dee512aea4
Filipe Moreira <fddcm.daniel@gmail.com>
2025-05-12 21:52:07 +0100

    feat: format the name to remove the pipes and get the first and last name

--- src/report.js
+++ src/report.js
@@ -48,7 +48,9 @@ export function generateStudentReport(studentsList, { includeAverage = true } = 
       average = undefined;
     }
 
-    const formattedName = name.trim();
+    const nameWithNoPipes = name.replace(/\|/g, '').trim();
+    const [firstName, ...rest] = nameWithNoPipes.split('_');
+    const formattedName = firstName + ' ' + rest.at(-1);
 
     reportList.push({
       id,
